6ecf58d6efae0fc0495c56919963edc4742ca3c4
chucho <jonathan.henao@outlook.es>
2019-09-20 17:00:45 -0500

    Secury example, register, login and Jwt

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        [StringLength(8, MinimumLength=4, ErrorMessage="Password must be between 4 and 8 characters")]
+        public string Password { get; set; }
+        
+
+       // [EmailAddress] Para cuando necesitemos validar el correo 
+    }
+}
