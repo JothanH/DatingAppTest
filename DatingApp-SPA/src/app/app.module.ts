1f8ddf432b002f1811964afd9ecdcf37450ee4c0
chucho <jonathan.henao@outlook.es>
2020-03-04 13:36:37 -0500

    sección 15 pagin, sorting and filtering

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angu
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule} from 'ngx-gallery';
@@ -67,6 +67,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
       FormsModule,
       ReactiveFormsModule,
       BsDropdownModule.forRoot(),
+      ButtonsModule.forRoot(),
+      PaginationModule.forRoot(),
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
       NgxGalleryModule,
--- DatingApp-SPA/src/app/memebers/memeber-list/memeber-list.component.ts
+++ DatingApp-SPA/src/app/memebers/memeber-list/memeber-list.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/models/user';
 import { AlertifyService } from '../../services/alertify.service';
 import { UserService } from '../../services/user.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/models/pagination';
 
 @Component({
   selector: 'app-memeber-list',
@@ -11,14 +12,48 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemeberListComponent implements OnInit {
    users: User[];
+   user: User = JSON.parse(localStorage.getItem('user'));
+   genderList = [{vale: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+   userParams: any = {};
+   pagination: Pagination;
 
   constructor(private userService: UserService, private alertify: AlertifyService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      // tslint:disable-next-line: no-string-literal
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
-  } 
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService
+        .getUsers( this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+        .subscribe( (res: PaginatedResult<User[]>) => {
+          this.users = res.result;
+          this.pagination = res.pagination;
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
+  }
 }
